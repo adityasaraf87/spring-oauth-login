4f02ea046956b6683d08ddeee93b4525f295e73e
aditya.saraf <adityasaraf87@gmail.com>
2020-01-17 09:15:55 +0530

    spring oauth

---
+++ src/test/java/com/spring/springoauthlogin/SpringOauthLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springoauthlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
